d09fa52f3ebf44dceb082f32b767d72926fb7b23
Nabilfadilah <mohammadfadilah00@gmail.com>
2025-08-22 20:20:09 +0700

    Redirect

---
+++ redirect-external.php
@@ -0,0 +1,5 @@
+<?php
+
+// redirect ke halaman google (udah beda domain)
+header('Location: https://www.google.com');
+exit();
---
+++ redirect.php
@@ -0,0 +1,5 @@
+<?php
+
+// pindah/redirect ke halaman phpinfo.php (masih didomain yang sama)
+header('Location: /phpinfo.php');
+exit();
